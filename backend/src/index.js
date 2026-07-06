b1c842d4f002cd67b50cd8be3b24771fe545d29b
successfulceo23 <uzihsamuel@gmail.com>
2026-07-06 14:44:32 +0100

    Fix build script for linux compatibility

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
-import "dotenv/config"
+import cors from "cors";
+
+import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
+import {clerkMiddleware} from "@clerk/express";
+
 import { connectDB } from "./lib/db.js";
 import User from "./models/user.model.js";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -15,6 +25,16 @@ app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
 app.listen(PORT, ()=>{
     connectDB();
-console.log("server is up and running:", PORT)})
+console.log("server is up and running:", PORT)});
